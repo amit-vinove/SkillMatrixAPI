a414aeef4a7c1838390f5b6f3dd0b0e709e2f98f
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-04-01 12:08:41 +0530

    Behaviour

--- SkillMatrix/Migrations/20220331105250_IN.cs
+++ SkillMatrix/Migrations/20220331105250_IN.cs
@@ -24,41 +24,6 @@ namespace SkillMatrix.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "Employees",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    EmployeeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ReportingManager = table.Column<int>(type: "int", nullable: false),
-                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Team = table.Column<int>(type: "int", nullable: false),
-                    Band = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Status = table.Column<bool>(type: "bit", nullable: false),
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Employees", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Teams",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Teams", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "TestDrivenDevelopments",
                 columns: table => new
                 {
@@ -86,21 +51,6 @@ namespace SkillMatrix.Migrations
                     table.PrimaryKey("PK_TestDrivenDevelopments", x => x.TestDrivenId);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Role = table.Column<int>(type: "int", nullable: false),
-                    RoleLevel = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.UserId);
-                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -109,16 +59,8 @@ namespace SkillMatrix.Migrations
                 name: "BehaviourDrivenDevelopments");
 
             migrationBuilder.DropTable(
-                name: "Employees");
-
-            migrationBuilder.DropTable(
-                name: "Teams");
-
-            migrationBuilder.DropTable(
                 name: "TestDrivenDevelopments");
 
-            migrationBuilder.DropTable(
-                name: "Users");
         }
     }
 }
