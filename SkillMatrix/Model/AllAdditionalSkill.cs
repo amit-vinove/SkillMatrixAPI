d13e779e882cdb8b5cf978e823e381b9a4b9d943
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-04-01 13:10:04 +0530

    Change in Add skills

--- SkillMatrix/Model/AllAdditionalSkill.cs
+++ SkillMatrix/Model/AllAdditionalSkill.cs
@@ -8,16 +8,7 @@ namespace SkillMatrix.Model
         public int AllAdditionalSkillId { get; set; }
         public int AdditionalSkillId { get; set; }
         public int EmpId { get; set; }        
-        public int SingleApiCall { get; set; }
-        public int ApolloExpress { get; set; }
-        public int DataLoader { get; set; }
-        public int ProressiveWebAppDev { get; set; }
-        public int Wpf { get; set; }
-        public int Sitecore { get; set; }
-        public int Xamarin { get; set; }
-        public int KenticoCms { get; set; }
-        public int UmbracoCms { get; set; }
-        public int Microservices { get; set; }
+        public string AllAdditionalSkillName { get; set; }
 
     }
 }
