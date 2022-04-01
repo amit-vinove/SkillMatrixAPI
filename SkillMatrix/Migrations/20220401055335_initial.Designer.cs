fb1076c1a69da8cb41b02d21002d655c29dbe8fd
Ruhi Peter <ruhipeter4@gmail.com>
2022-04-01 11:31:24 +0530

    DevopsWebserviceSqldever

--- SkillMatrix/Migrations/20220401055335_initial.Designer.cs
+++ SkillMatrix/Migrations/20220401055335_initial.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkillMatrix.Data;
 
@@ -10,9 +11,10 @@ using SkillMatrix.Data;
 namespace SkillMatrix.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220401055335_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SkillMatrix/Model/Devop.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillMatrix.Model
+{
+    public class Devop
+    {
+        [Key]
+        public int DevopId { get; set; }
+        public int DockerKubernetes { get; set; }
+        public int WebDb { get; set; }
+        public int CiCd { get; set; }
+        public int CiCdDevopPractice { get; set; }
+        public int EmpId { get; set; }
+    }
+}
