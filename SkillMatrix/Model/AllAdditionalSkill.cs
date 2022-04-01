c22af60e2a740f0436d85cef1321ec627e7052c7
Ruhi Peter <ruhipeter4@gmail.com>
2022-04-01 14:28:23 +0530

    rating

--- SkillMatrix/Model/AllAdditionalSkill.cs
+++ SkillMatrix/Model/AllAdditionalSkill.cs
@@ -9,6 +9,7 @@ namespace SkillMatrix.Model
         public int AdditionalSkillId { get; set; }
         public int EmpId { get; set; }        
         public string AllAdditionalSkillName { get; set; }
+        public int Rating { get; set; }
 
     }
 }
