a739a5005be83e9980885952df0798c9075a54c7
ayushmathur02 <ayush.mathur@mail.vinove.com>
2022-04-01 10:09:42 +0530

    BasicFoundation and GenericSKill

---
+++ SkillMatrix/Controllers/BasicFoundationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkillMatrix.Data;
+using SkillMatrix.Model;
+
+namespace SkillMatrix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicFoundationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public BasicFoundationController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+        [HttpGet("GetAllDetails")]
+
+        public async Task<IEnumerable<BasicFoundation>> GetAllDetails()
+        {
+            return _context.BasicFoundation.ToList();
+        }
+        [HttpPost("CreateBasicFoundation")]
+        public async Task<BasicFoundation> AddGenericSKills(BasicFoundation basicFoundation)
+        {
+            if (basicFoundation == null)
+                throw new ArgumentNullException("Add Basic Foundation");
+            _context.BasicFoundation.Add(basicFoundation);
+            await _context.SaveChangesAsync();
+            return basicFoundation;
+        }
+        [HttpDelete("DeleteBasicFoundationById")]
+        public async Task<bool> DeleteBasicFoundation(int basicFoundationId)
+        {
+            bool isDeleted = false;
+
+            if (basicFoundationId == 0)
+                return isDeleted;
+
+            var basicFoundation = await _context.BasicFoundation.FirstOrDefaultAsync(m => m.BasicFoundId == basicFoundationId);
+            if (basicFoundation == null)
+                return isDeleted;
+            _context.BasicFoundation.Remove(basicFoundation);
+            await _context.SaveChangesAsync();
+            isDeleted = true;
+            return isDeleted;
+        }
+    }
+}
---
+++ SkillMatrix/Model/BasicFoundation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillMatrix.Model
+{
+    public class BasicFoundation
+    {
+        [Key]
+        public int BasicFoundId { get; set; }
+        public string EmployeeId { get; set; }
+        public int TFS { get; set; }
+        public int GIT_OR_REVISION_CONTROL {get; set;}
+        public int JIRA_OR_AGILE_PRACTICES { get; set; }
+        public int SERVER_UPLOADS_OR_MANAGEMENT { get; set; }
+        public int BEST_PRACTICES { get; set;}
+        public int UNIT_TESTING { get; set; }
+
+    }
+}
